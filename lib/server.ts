f8a316435470c6ec075990b2f3f3265ab92a122c
SeNicko <sebastianflajszer@gmail.com>
2021-03-14 21:42:06 +0100

    tab width fix

--- lib/server.ts
+++ lib/server.ts
@@ -1,18 +1,18 @@
 import {
-	createServer,
-	Server,
-	IncomingMessage,
-	ServerResponse,
-	STATUS_CODES,
+  createServer,
+  Server,
+  IncomingMessage,
+  ServerResponse,
+  STATUS_CODES,
 } from 'http';
 
 import { compareUrl, getUrlParams } from './util/url';
 
 // Request type
 export interface Request<T = { [key: string]: string }>
-	extends IncomingMessage {
-	url: string;
-	params: T;
+  extends IncomingMessage {
+  url: string;
+  params: T;
 }
 
 // Response
@@ -22,89 +22,89 @@ export type RequestHandler = (req: Request, res: ServerResponse) => void;
 
 // RequestMeta structure
 interface RequestHandlerMeta {
-	path: string;
-	method: string;
-	handler: RequestHandler;
+  path: string;
+  method: string;
+  handler: RequestHandler;
 }
 
 // Util for parsing req body to string
 export const parseBody = (req: IncomingMessage): Promise<string> =>
-	new Promise<string>((res, rej) => {
-		let body = '';
+  new Promise<string>((res, rej) => {
+    let body = '';
 
-		req
-			.on('data', chunk => {
-				body += chunk;
-			})
-			.on('end', () => res(body))
-			.on('error', err => rej(err));
-	});
+    req
+      .on('data', chunk => {
+        body += chunk;
+      })
+      .on('end', () => res(body))
+      .on('error', err => rej(err));
+  });
 
 // App
 export class App {
-	private handlers: RequestHandlerMeta[] = [];
-	private server: Server = createServer(
-		(req: IncomingMessage, res: ServerResponse) =>
-			this.requestHandler(req as Request, res)
-	);
+  private handlers: RequestHandlerMeta[] = [];
+  private server: Server = createServer(
+    (req: IncomingMessage, res: ServerResponse) =>
+      this.requestHandler(req as Request, res)
+  );
 
-	constructor(private port: string | number) {}
+  constructor(private port: string | number) {}
 
-	private requestHandler(req: Request, res: ServerResponse) {
-		const { method, url } = req;
+  private requestHandler(req: Request, res: ServerResponse) {
+    const { method, url } = req;
 
-		// Find handler in reqister handlers array
-		const handler = this.handlers.find(
-			(handler: RequestHandlerMeta) =>
-				handler.method === method && compareUrl(handler.path, url)
-		);
+    // Find handler in reqister handlers array
+    const handler = this.handlers.find(
+      (handler: RequestHandlerMeta) =>
+        handler.method === method && compareUrl(handler.path, url)
+    );
 
-		// Check if handler exists
-		if (!handler) {
-			// If no return 404 message
-			res.writeHead(404);
-			res.end(STATUS_CODES[404]);
-		} else {
-			// Set defined params
-			req.params = getUrlParams(handler.path, url);
-			// Call main handler
-			handler.handler(req, res);
-		}
-	}
+    // Check if handler exists
+    if (!handler) {
+      // If no return 404 message
+      res.writeHead(404);
+      res.end(STATUS_CODES[404]);
+    } else {
+      // Set defined params
+      req.params = getUrlParams(handler.path, url);
+      // Call main handler
+      handler.handler(req, res);
+    }
+  }
 
-	private handle(path: string, method: string, handler: RequestHandler): void {
-		// Add new handler
-		this.handlers.push({
-			path,
-			method: method.toUpperCase(),
-			handler,
-		});
-	}
+  private handle(path: string, method: string, handler: RequestHandler): void {
+    // Add new handler
+    this.handlers.push({
+      path,
+      method: method.toUpperCase(),
+      handler,
+    });
+  }
 
-	// Get request handler
-	get(path: string, handler: RequestHandler): void {
-		this.handle(path, 'GET', handler);
-	}
+  // Get request handler
+  get(path: string, handler: RequestHandler): void {
+    this.handle(path, 'GET', handler);
+  }
 
-	// Post request handler
-	post(path: string, handler: RequestHandler): void {
-		this.handle(path, 'POST', handler);
-	}
+  // Post request handler
+  post(path: string, handler: RequestHandler): void {
+    this.handle(path, 'POST', handler);
+  }
 
-	// Put request handler
-	put(path: string, handler: RequestHandler): void {
-		this.handle(path, 'PUT', handler);
-	}
+  // Put request handler
+  put(path: string, handler: RequestHandler): void {
+    this.handle(path, 'PUT', handler);
+  }
 
-	// Delete request handler
-	delete(path: string, handler: RequestHandler): void {
-		this.handle(path, 'DELETE', handler);
-	}
+  // Delete request handler
+  delete(path: string, handler: RequestHandler): void {
+    this.handle(path, 'DELETE', handler);
+  }
 
-	listen(cb?: () => void): void {
-		// Start listening on specified port
-		this.server.listen(this.port);
-		// If there is a callback, fire it
-		if (cb) cb();
-	}
+  listen(cb?: () => void): void {
+    // Start listening on specified port
+    this.server.listen(this.port);
+    // If there is a callback, fire it
+    if (cb) cb();
+  }
 }
--- lib/util/url.ts
+++ lib/util/url.ts
@@ -1,39 +1,39 @@
 // Check if request url fit into schema
 export const compareUrl = (schema: string, url: string): boolean => {
-	// Remove query and split
-	const dividedSchema = schema.split('/');
-	const dividedUrl = url.split('?')[0].split('/');
+  // Remove query and split
+  const dividedSchema = schema.split('/');
+  const dividedUrl = url.split('?')[0].split('/');
 
-	// If length of urls is different return false
-	if (dividedSchema.length !== dividedUrl.length) return false;
+  // If length of urls is different return false
+  if (dividedSchema.length !== dividedUrl.length) return false;
 
-	// Check each url node
-	for (let i = 0; i < dividedSchema.length; i++) {
-		// If schema node does not start with semicolon, and nodes aren't equal return false
-		if (
-			dividedSchema[i].startsWith(':') === false &&
-			dividedSchema[i] !== dividedUrl[i]
-		)
-			return false;
-	}
+  // Check each url node
+  for (let i = 0; i < dividedSchema.length; i++) {
+    // If schema node does not start with semicolon, and nodes aren't equal return false
+    if (
+      dividedSchema[i].startsWith(':') === false &&
+      dividedSchema[i] !== dividedUrl[i]
+    )
+      return false;
+  }
 
-	// Url matches schema
-	return true;
+  // Url matches schema
+  return true;
 };
 
 // Get params from a url
 export const getUrlParams = (
-	schema: string,
-	url: string
+  schema: string,
+  url: string
 ): { [key: string]: string } => {
-	// Declare final object
-	const params: { [key: string]: string } = {};
+  // Declare final object
+  const params: { [key: string]: string } = {};
 
-	const dividedUrl = url.split('/');
-	schema.split('/').forEach((node, i) => {
-		// Iterate through url and find params
-		if (node.startsWith(':')) params[node.split(':')[1]] = dividedUrl[i];
-	});
+  const dividedUrl = url.split('/');
+  schema.split('/').forEach((node, i) => {
+    // Iterate through url and find params
+    if (node.startsWith(':')) params[node.split(':')[1]] = dividedUrl[i];
+  });
 
-	return params;
+  return params;
 };
